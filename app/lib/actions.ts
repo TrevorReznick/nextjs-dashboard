7dee1a4b2a7f4cdbcf46d6c200ce8054e59b6be9
Vincent Maximo II <projects.manager@xdomain.it>
2024-01-06 14:12:38 +0100

    fixing error validation, to do other validations fields

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -53,7 +53,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
- 
+
   // Prepare data for insertion into the database
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
