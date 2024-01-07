5da51f39ba29513deedcfe23df2825c942b8dfc6
Vincent Maximo II <projects.manager@xdomain.it>
2024-01-07 20:21:55 +0100

    testing metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,26 @@
-import '@/app/ui/global.css'
-import { inter } from '@/app/ui/fonts'
- 
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL(
+    'https://nextjs-dashboard-nfemf0zmi-double-facessss-projects.vercel.app/',
+  ),
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
